2c6aec4b52c2bddd0f439deaa8a6d1db21773ece
François Sestier <francois.sestier@gmail.com>
2021-05-05 18:06:34 +0200

    vector_tuple utilities is vector / VectorTuple concept get(VectorTuple v)<T> returns vector of type vector<T> form tuple v;

---
+++ antity/src/core/Utility/VectorTuple.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "TypeTraits.h"
+
+template<typename ...Ts>
+struct vector_tuple {
+	using type = std::tuple<std::vector<Ts>...>;
+};
+
+template<typename ...Ts>
+struct is_vector_tuple : std::false_type {};
+
+template<typename ...Ts>
+struct is_vector_tuple <std::tuple<std::vector<Ts>...>> : std::true_type {};
+
+
+template<typename T>
+concept Vector_Tuple = is_vector_tuple<T>::value;
+
+template <typename  T, Vector_Tuple VectorTuple >
+std::vector<T>& get(VectorTuple vectorTuple) {
+	return std::get<std::vector<T>, vectorTuple>;
+};
