423debc0136f0ee6e73745cd547ba401382cb2ab
François Sestier <francois.sestier@gmail.com>
2021-05-10 23:11:42 +0200

    Unit Test Remove Component passing

--- antity/src/antity.cpp
+++ antity/src/antity.cpp
@@ -9,33 +9,29 @@ using namespace ant;
 
 int main()
 {
+	Registry reg;
+
+	reg.RegisterComponent<int>();
+	reg.RegisterComponent<float>();
+	reg.RegisterComponent<char>();
+
+	Entity entity = reg.CreateEntity();
+	Entity other = reg.CreateEntity();
+
+	reg.AddComponent<int>(entity, 1);
+	reg.AddComponent<int>(other, 10);
+	reg.AddComponent<float>(entity, .4f);
+	reg.AddComponent<char>(other, 'c');
+
+	int counter = 0;
+
+	for (auto [e, i, f] : reg.GetComponents<int, float>())
 	{
-		Registry reg;
 
-		ChunkID id = 0;
-
-		reg.RegisterComponent<int>();
-		reg.RegisterComponent<float>();
-		reg.RegisterComponent<char>();
-
-		Entity entity = reg.CreateEntity();
-		Entity other = reg.CreateEntity();
-
-		reg.AddComponent<int>(entity, id, 1);
-		reg.AddComponent<int>(other, id, 10);
-		reg.AddComponent<float>(entity, id, .4f);
-		reg.AddComponent<float>(other, id, .5f);
-		reg.AddComponent<char>(other, id, 'c');
-
-		int counter = 0;
-
-		for (auto [e, i, f] : reg.GetComponents<int, float>())
-		{
-
-			counter++;
-			f = .5f;
-		}
-
-		auto [j] = reg.GetEntityComponents<float>(entity);
+		counter++;
+		f = .5f;
 	}
+
+	auto [j] = reg.GetEntityComponents<float>(entity);
+
 }
