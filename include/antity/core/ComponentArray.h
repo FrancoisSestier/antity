28912cf326d2fb2209f89693afef34a82a5fe57c
FrancoisSestier <francois.sestier@gmail.com>
2021-06-10 17:45:28 +0200

    Component Array Refactoring :
    Replace  Iterator with custom ComponentIterator since Iterator.hpp util has been removed from utils few commits ago to get rid of unused code.

--- include/antity/core/ComponentArray.h
+++ include/antity/core/ComponentArray.h
@@ -2,31 +2,109 @@
 
 namespace ant {
 
-    template <typename T>
+    template <typename C>
     struct ComponentArray {
-        using iterator = Iterator<T>;
+        struct ComponentIterator {
+            using iterator_category = std::random_access_iterator_tag;
+            using difference_type = std::ptrdiff_t;
+            using value_type = C;
+            using pointer = C*;    // or also value_type*
+            using reference = C&;  // or also value_type&
 
+            ComponentIterator() : m_ptr(nullptr) {}
+            ComponentIterator(C* rhs) : m_ptr(rhs) {}
+            ComponentIterator(const ComponentIterator& rhs) : m_ptr(rhs.m_ptr) {}
+
+            pointer get() { return m_ptr; }
+
+            reference operator*() const { return *m_ptr; }
+            pointer operator->() { return m_ptr; }
+
+            difference_type operator-(const ComponentIterator& rhs) {
+                return m_ptr - rhs.m_ptr;
+            }
+            ComponentIterator operator+(const difference_type& rhs) {
+                return ComponentIterator(m_ptr + rhs);
+            }
+            ComponentIterator operator-(const difference_type& rhs) {
+                return ComponentIterator(m_ptr - rhs);
+            }
+            ComponentIterator& operator+=(const difference_type& rhs) {
+                m_ptr += rhs;
+                return *this;
+            }
+            ComponentIterator& operator-=(const difference_type& rhs) {
+                m_ptr -= rhs;
+                return *this;
+            }
+
+            reference operator[](const difference_type& rhs) {
+                return m_ptr[rhs];
+            }
+
+            ComponentIterator& operator++() {
+                m_ptr++;
+                return *this;
+            }
+            ComponentIterator operator++(int) {
+                ComponentIterator tmp = *this;
+                ++(*this);
+                return tmp;
+            }
+            ComponentIterator& operator--() {
+                m_ptr--;
+                return *this;
+            }
+            ComponentIterator operator--(int) {
+                ComponentIterator tmp = *this;
+                --(*this);
+                return tmp;
+            }
+
+            bool operator>(const ComponentIterator& rhs) const {
+                return m_ptr > rhs.m_ptr;
+            }
+            bool operator<(const ComponentIterator& rhs) const {
+                return m_ptr < rhs.m_ptr;
+            }
+            bool operator>=(const ComponentIterator& rhs) const {
+                return m_ptr >= rhs.m_ptr;
+            }
+            bool operator<=(const ComponentIterator& rhs) const {
+                return m_ptr <= rhs.m_ptr;
+            }
+
+            friend bool operator==(const ComponentIterator& a, const ComponentIterator& b) {
+                return a.m_ptr == b.m_ptr;
+            };
+            friend bool operator!=(const ComponentIterator& a, const ComponentIterator& b) {
+                return a.m_ptr != b.m_ptr;
+            };
+
+           private:
+            pointer m_ptr;
+        };
         ComponentArray() = default;
 
         ComponentArray(ByteArray* byteArray)
-            : data(reinterpret_cast<T*>(byteArray->componentData)),
+            : data(reinterpret_cast<C*>(byteArray->componentData)),
               byteArray(byteArray) {}
 
-        [[nodiscard]] const T& operator[](size_t index) const {
+        [[nodiscard]] const C& operator[](size_t index) const {
             return data[index];
         }
 
-        [[nodiscard]] T& operator[](size_t index) { return data[index]; }
+        [[nodiscard]] C& operator[](size_t index) { return data[index]; }
 
         bool valid() const noexcept { return data != nullptr; }
 
-        T* begin() { return data; }
+        C* begin() { return data; }
 
-        T* end() { return data + byteArray->size; }
+        C* end() { return data + byteArray->size; }
 
         int16_t size() { return byteArray->size; }
 
-        T* data{nullptr};
+        C* data{nullptr};
         ByteArray* byteArray{nullptr};
     };
 
