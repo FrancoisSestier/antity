387e2112d5bbbea7fadcc4abd2499c3358932924
FrancoisSestier <francois.sestier@gmail.com>
2021-06-10 17:47:23 +0200

    Identifier Refactoring
    -changed typedefs to using keyword to acheive better code consistency
    -added Signature type wich is a bitset of size max components

--- include/antity/core/Identifier.h
+++ include/antity/core/Identifier.h
@@ -5,14 +5,15 @@
 
 namespace ant {
 
-    typedef uint32_t IDType;
-    typedef IDType ComponentTypeID;
-    typedef IDType Entity;
-    typedef IDType ChunkID;
+    using IDType = uint32_t;
+    using ComponentTypeID = IDType;
+    using Entity = IDType;
+    using ChunkID = IDType;
     inline constexpr IDType NULL_ENTITY = 0;
     inline constexpr IDType NULL_CHUNK = UINT32_MAX;
     inline constexpr uint16_t MAX_COMPONENTS = 64;
-    typedef std::vector<ComponentTypeID> ArchetypeID;
+    using Signature = std::bitset<MAX_COMPONENTS>;
+    using ArchetypeID = std::vector<ComponentTypeID>;
 
     class TypeIdGenerator {
         inline static IDType identifier() noexcept {
@@ -34,12 +35,12 @@ namespace ant {
         }
     };
 
-    inline static auto GetTypeSignature(IDType Id) noexcept {
-        return std::bitset<MAX_COMPONENTS>(1ULL << static_cast<uint64_t>(Id));
+    inline Signature GetTypeSignature(IDType Id) noexcept {
+        return Signature(1ULL << static_cast<uint64_t>(Id));
     }
 
     template <typename T>
-    inline static auto GetTypeSignature() noexcept {
+    inline Signature GetTypeSignature() noexcept {
         return GetTypeSignature(TypeIdGenerator::GetTypeID<T>());
     }
 }  // namespace ant
